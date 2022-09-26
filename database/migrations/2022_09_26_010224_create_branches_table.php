d884b0b579ee9eac731990d77b8e8970f1e88c5c
Fabricio-Guima <fsgkof@gmail.com>
2022-09-25 22:05:09 -0300

    model migration controller branch (filial)

---
+++ database/migrations/2022_09_26_010224_create_branches_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('direction')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('document')->nullable();
+            $table->string('printer')->nullable();
+            $table->string('url_printer')->nullable();
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branches');
+    }
+};
