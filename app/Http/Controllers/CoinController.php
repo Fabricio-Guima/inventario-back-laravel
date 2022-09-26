add7ea74534af2d55e459e2161c5a2964bdf2c36
Fabricio-Guima <fsgkof@gmail.com>
2022-09-25 21:57:04 -0300

    model migration controller coins

---
+++ app/Http/Controllers/CoinController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coin;
+use Illuminate\Http\Request;
+
+class CoinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Coin  $coin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Coin $coin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Coin  $coin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Coin $coin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Coin  $coin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Coin $coin)
+    {
+        //
+    }
+}
