5bc4ed265a162fdc5e787bb8bf083d10088057ea
Fabricio-Guima <fsgkof@gmail.com>
2022-09-25 21:59:46 -0300

    migration coin_image

---
+++ database/migrations/2022_09_26_005853_create_coin_image_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coin_image', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('coin_id')->nullable()->constrained('coins');
+            $table->foreignId('image_id')->nullable()->constrained('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coin_image');
+    }
+};
